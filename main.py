9796978b7fadceca1208644c81963526d45bfd11
Fumbre <ernarovskiyvlad@gmail.com>
2026-03-11 14:09:55 +0100

    add: embedded/week_5 & solo/embedded

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-# graph logic
 import networkx as nx
-
-# visualization
-import matplotlib.pyplot as plt
 from pyvis.network import Network
+import matplotlib.pyplot as plt
 
-import math
+G = nx.Graph()
+G.add_edge("A", "B")
+
+G.add_edge("B", "Ab")
+G.add_edge("B", "Bb")
+G.add_edge("B", "Cb")
+
+
+G.add_edge("A", "C")
+G.add_edge("C", "Aa")
+G.add_edge("C", "Ba")
+
+G.add_edge("D", "Ab")
+G.add_edge("D", "Bb")
+G.add_edge("D", "Cb")
+
+# html
+
+net = Network(notebook=False, directed=False)
+net.from_nx(G)
+
+try:
+  net.save_graph("graph.html")
+  print("Файл успешно создан: graph.html")
+except Exception as e:
+  print(f"Ошибка: {e}")
+
+# plt
+nx.draw(G, with_labels=True, node_color='skyblue', node_size=800)
+plt.show()
